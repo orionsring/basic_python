02daaf396c646eff3daf4fb2ad0943bda60e981d
Donny <orionsring@tamu.edu>
2019-05-30 13:19:25 -0600

    Add files via upload

---
+++ urlopen.py
@@ -0,0 +1,12 @@
+
+from urllib.request import urlopen
+
+conn = urlopen('http://codeschool.com')
+conn.status
+conn.reason
+lines = conn.readlines()
+
+data=''.join([line.decode('utf-8') for line in lines])
+# do something with the data
+conn.close()
+
