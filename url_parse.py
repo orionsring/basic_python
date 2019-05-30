e17220345ec83a838f8d4daa39b81f298c7c0502
Donny <orionsring@tamu.edu>
2019-05-30 13:49:42 -0600

    Add files via upload

--- url_parse.py
+++ url_parse.py
@@ -5,10 +5,7 @@ result.scheme    #http
 result.path      # /list 
 result.query     # sort=price&filter=type
 
-from urllib.parse import urlparse, parse_qs
-url = 'http://example.com/list?sort=price&filter=type'
-result = urlparse(url)
-qs_data= parse_qs(result.query)
+
 
 
 
